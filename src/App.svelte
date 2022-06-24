7839153f8ef0e498da27ca1d4fc8b05dcbfce72f
Maya Mircheva <may4eto@hotmail.com>
2022-06-24 18:50:41 +0200

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,22 @@
+<script>
+	import Library from "./library/Library.svelte"
+	export let name;
+</script>
+
+<main>
+	<Library />
+</main>
+
+<style>
+	main {
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
